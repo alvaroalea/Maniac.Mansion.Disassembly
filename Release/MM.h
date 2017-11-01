1fee611b80b81f7aba1a0ca8c9d74040f2ae70c3
Alvaro Alea <alvaroalea@gmail.com>
2017-11-02 00:39:45 +0100

    start to work on objects

---
+++ Release/MM.h
@@ -0,0 +1,53 @@
+// Direction	Value
+#define DIR_Left	4
+#define DIR_Right	5
+#define DIR_Camera	6
+#define DIR_Away	7
+	
+	
+// Verbs	Value
+#define VER_None	0
+#define VER_Open	1
+#define VER_Close	2
+#define VER_Give	3
+#define VER_TurnOn	4
+#define VER_TurnOff 5
+#define VER_Fix		6
+#define VER_NewKid	7
+#define VER_Unlock	8
+#define VER_Push	9
+#define VER_Pull	0xA
+#define VER_Use		0xB
+#define VER_Read	0xC
+#define VER_WalkTo	0xD
+#define VER_PickUp	0xE
+#define VER_WhatIs	0xF
+#define VER_Lock    0x10  // no en excel, solo en ObjectScripts.c
+
+
+// Actors	
+#define ACT_Syd	1
+#define ACT_Razor	2
+#define ACT_Dave	3
+#define ACT_Michael	4
+#define ACT_Bernard	5
+#define ACT_Wendy	6
+#define ACT_Jeff	7
+#define ACT_Radiation_Suit	8
+#define ACT_Dr_Fred	9
+#define ACT_Nurse_Edna	10
+#define ACT_Weird_Ed	11
+#define ACT_Dead_Cousin_Ted	12
+#define ACT_Purple_Tentacle	13
+#define ACT_Green_Tentacle	14
+#define ACT_Meteor_police	15
+#define ACT_Meteor	16
+#define ACT_Mark_Eteer	17
+#define ACT_Talkshow_Host	18
+#define ACT_Plant	19
+#define ACT_Meteor_Radiation	20
+#define ACT_Edsel	21 // (small, outro)
+#define ACT_Meteor_s 	22 // (small, intro)
+#define ACT_Sandy_Lab	23
+#define ACT_Sandy_CutScene	24
+
